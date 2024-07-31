9b6a67d0f4e728dbe2d4363a88aaa44d9ecdc732
MD.Taohid Imam Khan Tamim <61451847+tamim-18@users.noreply.github.com>
2024-07-31 11:13:24 +0600

    Update 01_longest_common_subsequence.cpp

--- interview/dp/LCS/01_longest_common_subsequence.cpp
+++ interview/dp/LCS/01_longest_common_subsequence.cpp
@@ -6,21 +6,40 @@ public:
         int n = text1.size();
         int m = text2.size();
         int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==0 or j==0) dp[i][j]=0;
-            }
-        }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        
+        // Initialize the dp array
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) dp[i][j] = 0;
             }
         }
 
+        // Fill the dp array
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        // Reconstruct the LCS from the dp array
+        string lcs = "";
+        int i = n, j = m;
+        while (i > 0 && j > 0) {
+            if (text1[i-1] == text2[j-1]) {
+                lcs = text1[i-1] + lcs;
+                i--;
+                j--;
+            } else if (dp[i-1][j] > dp[i][j-1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        cout << "LCS: " << lcs << endl;
         return dp[n][m];
     }
-
 };
 
 // memorization  
