95ec27ad74ac6d65331271fb1026ca806611251d
MD.Taohid Imam Khan Tamim <61451847+tamim-18@users.noreply.github.com>
2024-08-14 22:11:02 +0600

    Update 01_N_meetings_in_one_room.cpp

--- interview/Greedy/01_N_meetings_in_one_room.cpp
+++ interview/Greedy/01_N_meetings_in_one_room.cpp
@@ -9,15 +9,15 @@ class Solution
         vector<pair<int,int>>v;
         for(int i=0;i<n;i++) v.push_back({start[i],end[i]});
         sort(v.begin(),v.end(),cmp);
-        int cnt=1;
+        int cnts=1;
         int lastEnd=v[0].second;
         for(int i=1;i<n;i++){
             if(v[i].first>lastEnd) {
-                cnt++;
+                cnts++;
                 lastEnd=v[i].second;
             }
         }
-        return cnt;
+        return cnts;
     }
     static bool cmp(const pair<int,int>&a, const pair<int,int>&b) 
         {return a.second<b.second;}
